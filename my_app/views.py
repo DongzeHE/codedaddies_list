436bc5fcad0b9692add7145198e0ce7c9986857f
Dongze He <dhe17@umd.edu>
2020-05-30 14:38:00 -0400

    Add craiglist functionality

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
+import re
+
 import requests
+
+CRAIGSLIST_URL = 'https://washingtondc.craigslist.org/search/?query={}'
+IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 
 # Create your views here.
 def home(request):
@@ -9,8 +16,36 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = CRAIGSLIST_URL.format(requests.compat.quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_url, post_image_url, post_title, post_price))
+
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
 
